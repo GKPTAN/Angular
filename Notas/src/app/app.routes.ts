13d7a3ec923a5b14bc00a4cdcf3ea927025c675a
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-06-23 23:39:16 -0300

    update
    - não sei se isso vai funcionar, mas vamos lá

--- Notas/src/app/app.routes.ts
+++ Notas/src/app/app.routes.ts
@@ -1,5 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ListaDeNotasComponent } from './lista-de-notas/lista-de-notas.component';
+import { AdicionarNotasComponent } from './adicionar-notas/adicionar-notas.component';
+import { DetalhesDaNotaComponent } from './detalhes-da-nota/detalhes-da-nota.component';
 
 export const routes: Routes = [
-    
+  { path: '', component: ListaDeNotasComponent },
+  { path: 'new', component: AdicionarNotasComponent },
+  { path: 'nota/:id', component: DetalhesDaNotaComponent },
 ];
+
+@NgModule ({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- Notas/src/main.ts
+++ Notas/src/main.ts
@@ -2,19 +2,12 @@ import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter, Routes } from '@angular/router'
-import { ListaDeNotasComponent } from './app/lista-de-notas/lista-de-notas.component';
-import { AdicionarNotasComponent } from './app/adicionar-notas/adicionar-notas.component';
-import { DetalhesDaNotaComponent } from './app/detalhes-da-nota/detalhes-da-nota.component';
-
-const routes: Routes = [
-  { path: '', component: ListaDeNotasComponent},
-  { path: 'new', component: AdicionarNotasComponent},
-  { path: 'nota/:id', component: DetalhesDaNotaComponent}
-];
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { AppRoutingModule } from './app/app.routes';
 
 const combinedConfig = {
-  providers: [provideRouter(routes)], appConfig
+  providers: [provideRouter(routes)], AppRoutingModule, appConfig
 };
 
 bootstrapApplication(AppComponent, combinedConfig)
